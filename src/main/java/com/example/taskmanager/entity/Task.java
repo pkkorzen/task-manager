62469ebe362f85289174c63ca4cb86592a4d70b5
Przemek <pk.korzen1@gmail.com>
2018-10-26 15:46:07 +0200

    initial commit - Task entity added

---
+++ src/main/java/com/example/taskmanager/entity/Task.java
@@ -0,0 +1,51 @@
+package com.example.taskmanager.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String descrpition;
+    private boolean isDone;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescrpition() {
+        return descrpition;
+    }
+
+    public void setDescrpition(String descrpition) {
+        this.descrpition = descrpition;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
